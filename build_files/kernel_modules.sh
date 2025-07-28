7ede87802464dce2e62ec9468ec63953bcc0c817
phantomcortex <phantomcortex@protonmail.com>
2025-07-27 22:46:36 -0600

    xpadneo kmod attempt #9 (my own typos will be the death of me)

--- build_files/kernel_modules.sh
+++ build_files/kernel_modules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e ("\033[31mINSTALL MODULES\033[0m")
+echo -e "\033[31mINSTALL MODULES\033[0m"
 dnf5 -y install dkms 
 # First, ensure the proper kernel-devel package is available
 # You'll need the Bazzite kernel headers specifically
@@ -9,12 +9,12 @@ KERNEL=$(ls /lib/modules/ | grep bazzite | sort -V | tail -1)
 # Set up the build environment properly
 export KERNELDIR="/lib/modules/${KERNEL}/build"
 
-PREV_DIR=$(pwd) && echo -e ("\033[33m$pwd\033[0m")
+PREV_DIR=$(pwd) && echo -e "\033[33m$pwd\033[0m"
 git clone https://github.com/atar-axis/xpadneo.git /tmp/xpadneo
 cd /tmp/xpadneo/hid-xpadneo
 #modified straight from xpadneo's makefile
 
-echo -e ("\033[37m$tee\033[0m")
+echo -e "\033[37m$tee\033[0m"
 tee makefile << 'EOF'
 KERNEL_SOURCE_DIR ?= /lib/modules/$(shell ls /lib/modules/ | tail -1)/build
 LD := ld.bfd
@@ -41,11 +41,11 @@ reinstall: modules
 dkms.conf: dkms.conf.in ../VERSION
 	sed 's/"@DO_NOT_CHANGE@"/"$(shell cat ../VERSION)"/g' <"$<" >"$@" 
 EOF
-echo -e ("\033[31mMAKE MODULES\033[0m")
+echo -e "\033[31mMAKE MODULES\033[0m"
 make modules 
-echo -e ("\033[31mMODULES_INSTALL\033[0m")
+echo -e "\033[31mMODULES_INSTALL\033[0m"
 make modules_install
-echo -e ("\033[31mMODULES DONE\033[0m")
+echo -e "\033[31mMODULES DONE\033[0m"
 sleep 5
 
 FILE1="/lib/modules/${KERNEL}/extra/xpadneo/xpadneo.ko.zst"
