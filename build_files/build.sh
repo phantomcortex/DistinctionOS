235053ce694a375c7c2ecb5d169a76fd7dd04bcf
phantom <137958098+phantomcortex@users.noreply.github.com>
2025-08-04 03:15:27 -0600

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,14 +3,14 @@
 set -ouex pipefail
 
 ### Install packages
-RED='\033[31m'
-NC='\033[0m'
+
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
+
 flatpak remote−add −−if−not−exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak_apps=(
@@ -33,7 +33,8 @@ for app in "${flatpak_apps[@]}"; do
     flatpak install -y --noninteractive flathub "$app"
 done
 
-
+RED='\033[31m'
+NC='\033[0m'
 # Define the COPR repo and package
 dnf5 -y config-manager setopt "*rpmfusion*".enabled=1 #bazzite seems to have some of rpmfusion repos disabled from their base images; Words cannot accurately describe how much this infuriated me  
 if ! dnf repolist | grep -q "copr:copr.fedorainfracloud.org:bazzite-org:rom-properties"; then
