cf17d16f7c32df0e006756e688984612237185a5
phantom <137958098+phantomcortex@users.noreply.github.com>
2025-08-07 07:44:20 -0600

    attempt fix to crossover #4

--- build_files/fix_opt.sh
+++ build_files/fix_opt.sh
@@ -6,6 +6,8 @@ set -euo pipefail
 #==Crossover
 RED="\033[31m"
 NC="\033[0m"
+GREEN="\033[32m"
+CYAN="\033[36m"
 #rm -rf /opt
 # remove link so installing crossover is possible
 if [ -L "/opt" ] && [ -d "/var/opt" ]; then
@@ -18,18 +20,24 @@ echo "mainline remove opt"
 dnf -y install http://crossover.codeweavers.com/redirect/crossover.rpm
 #if [[ -d /opt/cxoffice ]]; then
 echo "$RED DEBUG:$NC"
-ls /opt
-ls /var/opt
+echo -e "$GREEN ls /opt >> $NC"
+ls /opt 
+echo -e "$GREEN ls /var/opt >> $NC"
+if [[ ! -d /var/opt ]]; then
+  echo -e "$RED /var/opt does not exist for some reason...\n $CYAN CREATING... $NC"
+  mkdir -p /var/opt
+ls /var/opt 
 opt_empty=true
 #
 if [ "$(find "/opt" -mindepth 1 -print -quit)" ]; then
-    echo "$RED DEBUG: /opt is not empty"
+    echo "$RED DEBUG: /opt is not empty $NC"
     opt_empty=false
     mkdir -p /var/opt
     mv /opt/cxoffice /var/opt
 else
   mv /opt/*/ /var/opt
 fi
+
 if [ "$opt_empty" = true ]; then
     echo "${RED}nothing in opt. Something went wrong." >&2
     exit 1
