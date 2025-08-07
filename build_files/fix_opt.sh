0215c260c4a913816d34a4f75c21903572109d9b
phantomcortex <phantomcortex@protonmail.com>
2025-08-07 10:24:36 -0600

    I'm sure something will break as usual

--- build_files/fix_opt.sh
+++ build_files/fix_opt.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
 set -euo pipefail
 
-# Credit: https://github.com/astrovm/amyos/blob/main/build_files/fix-opt.sh
-
 #==Crossover
 RED="\033[31m"
 NC="\033[0m"
@@ -49,46 +47,4 @@ if [ "$recreate_opt" = true ]; then
   echo "ln -s /opt /var/opt"
   ln -s /opt /var/opt
 fi
- 
-#echo -e ""
-#mv /opt/cxoffice /usr/lib/opt
-# Crossover Requires a license file so It needs to be writable
-# in theory this should be handled in fix_opt.sh
-# relink
-#rm -rf /opt
-#ln -s /opt /var/opt
 
-
-trap '[[ $BASH_COMMAND != echo* ]] && [[ $BASH_COMMAND != log* ]] && echo "+ $BASH_COMMAND"' DEBUG
-
-log() {
-  echo "=== $* ==="
-}
-
-log "Starting /opt directory fix"
-
-# Move directories from /var/opt to /usr/lib/opt
-
-: '
-for dir in /var/opt/*/; do
-  [ -d "$dir" ] || continue
-  dirname=$(basename "$dir")
-  mv "$dir" "/usr/lib/opt/$dirname"
-  # Since I like to support wine developtment by buying Crossover -
-  # I often have to use hacky workarounds on immutable systems
-  if [["$dirname" == "cxoffice"]]; then
-    echo "d /usr/lib/opt/$dirname 0755 root root -" >> /usr/lib/tmpfiles.d/distinction-opt-fix.conf
-    echo "d /usr/lib/opt/$dirname/etc 0755 root root -" >> /usr/lib/tmpfiles.d/distinction-opt-fix.conf
-    echo "f /usr/lib/opt/$dirname/etc/cxoffice.conf 0644 root root -" >> /usr/lib/tmpfiles.d/distinction-opt-fix.conf
-    echo "f /usr/lib/opt/$dirname/etc/license.txt 0644 root root -" >> /usr/lib/tmpfiles.d/distinction-opt-fix.conf
-    for subdir in bin lib lib64 share support; do 
-      if [[ -d "/usr/lib/opt/$dirname/$subdir" ]]; then
-        echo "L+ /var/opt/$dirname/$subdir - - - - /usr/lib/opt/$dirname/$subdir" >> /usr/lib/tmpfiles.d/distinction-opt-fix.conf
-      fi 
-      echo -e "\033[31mDEBUG: \033[36mvar_opt:\033[32m$(ls /var/opt/) \033[36musr_lib_opt:\033[32m$(ls /usr/lib/opt) \n \033[31m$(cat /usr/lib/tmpfiles.d/distinction-opt-fix.conf)\033[0m"
-    done
-  fi
-  echo "L+ /var/opt/$dirname - - - - /usr/lib/opt/$dirname" >>/usr/lib/tmpfiles.d/distinction-opt-fix.conf
-done
-'
-log "Fix completed"
