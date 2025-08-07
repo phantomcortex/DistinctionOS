29e5e61362d135432d793d3a814f4b6de7127558
phantomcortex <phantomcortex@protonmail.com>
2025-08-07 10:42:13 -0600

    might fix...but I'm sure this will be my 13th reason

--- build_files/fix_opt.sh
+++ build_files/fix_opt.sh
@@ -8,22 +8,32 @@ GREEN="\033[32m"
 CYAN="\033[36m"
 #rm -rf /opt
 # remove link so installing crossover is possible
+if [[ ! -d /var/opt ]]; then
+  echo -e "$RED /var/opt does not exist for some reason...\n $CYAN CREATING... $NC"
+  mkdir -p /var/opt
+fi #
+
+
 if [ -L "/opt" ] && [ -d "/var/opt" ]; then
     echo -e "DEBUG: /opt is a symlink to /var/opt"
-    rm -rf /opt
-    recreate_opt=true
-fi
-rm -rf /opt
-echo "mainline remove opt"
-dnf -y install http://crossover.codeweavers.com/redirect/crossover.rpm
+    #rm -rf /opt
+    #recreate_opt=true
+else 
+    echo "DEBUG: /opt is not a symlink to /var/opt"
+fi #
+
+
+#echo "mainline remove opt"
+#try to install to /var/opt
+dnf install -y http://crossover.codeweavers.com/redirect/crossover.rpm || { echo -e "$RED dnf failed, running fallback...$NC"; rm -rf /opt/ /var/opt && mkdir /opt/cxoffice /var/opt && dnf install -y http://crossover.codeweavers.com/redirect/crossover.rpm || echo "ok.... what now?" && exit 1; }
+#dnf -y install http://crossover.codeweavers.com/redirect/crossover.rpm
 #if [[ -d /opt/cxoffice ]]; then
 echo -e "$RED DEBUG:$NC"
 echo -e "$GREEN ls /opt >> $NC"
 ls /opt 
 echo -e "$GREEN ls /var/opt >> $NC"
-if [[ ! -d /var/opt ]]; then
-  echo -e "$RED /var/opt does not exist for some reason...\n $CYAN CREATING... $NC"
-  mkdir -p /var/opt
+#
+
 ls /var/opt 
 opt_empty=true
 #
@@ -39,12 +49,47 @@ fi
 if [ "$opt_empty" = true ]; then
     echo "${RED}nothing in opt. Something went wrong." >&2
     exit 1
-fi
-
+fi #
+#
 if [ "$recreate_opt" = true ]; then
   echo "$RED rm opt$NC"
   rm -rf /opt 
   echo "ln -s /opt /var/opt"
   ln -s /opt /var/opt
-fi
+fi #
+
+
+trap '[[ $BASH_COMMAND != echo* ]] && [[ $BASH_COMMAND != log* ]] && echo "+ $BASH_COMMAND"' DEBUG
+
+log() {
+  echo "=== $* ==="
+}
+
+log "Starting /opt directory fix"
+
+# Move directories from /var/opt to /usr/lib/opt
+
+
+for dir in /var/opt/*/; do
+  [ -d "$dir" ] || continue
+  dirname=$(basename "$dir")
+  mv "$dir" "/usr/lib/opt/$dirname"
+  # Since I like to support wine developtment by buying Crossover -
+  # I often have to use hacky workarounds on immutable systems
+  if [["$dirname" == "cxoffice"]]; then
+    echo "d /usr/lib/opt/$dirname 0755 root root -" >> /usr/lib/tmpfiles.d/distinction-opt-fix.conf
+    echo "d /usr/lib/opt/$dirname/etc 0755 root root -" >> /usr/lib/tmpfiles.d/distinction-opt-fix.conf
+    echo "f /usr/lib/opt/$dirname/etc/cxoffice.conf 0644 root root -" >> /usr/lib/tmpfiles.d/distinction-opt-fix.conf
+    echo "f /usr/lib/opt/$dirname/etc/license.txt 0644 root root -" >> /usr/lib/tmpfiles.d/distinction-opt-fix.conf
+    for subdir in bin lib lib64 share support; do 
+      if [[ -d "/usr/lib/opt/$dirname/$subdir" ]]; then
+        echo "L+ /var/opt/$dirname/$subdir - - - - /usr/lib/opt/$dirname/$subdir" >> /usr/lib/tmpfiles.d/distinction-opt-fix.conf
+      fi 
+      echo -e "\033[31mDEBUG: \033[36mvar_opt:\033[32m$(ls /var/opt/) \033[36musr_lib_opt:\033[32m$(ls /usr/lib/opt) \n \033[31m$(cat /usr/lib/tmpfiles.d/distinction-opt-fix.conf)\033[0m"
+    done
+  fi
+  echo "L+ /var/opt/$dirname - - - - /usr/lib/opt/$dirname" >>/usr/lib/tmpfiles.d/distinction-opt-fix.conf
+done
+
+log "Fix completed"
 
