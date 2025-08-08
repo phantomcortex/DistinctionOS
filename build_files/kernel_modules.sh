07edb6d157d210bdb206c9de0e8210dd5c171b2a
phantomcortex <phantomcortex@protonmail.com>
2025-08-08 03:04:36 -0600

    better build script now instated as main

--- build_files/kernel_modules.sh
+++ build_files/kernel_modules.sh
@@ -7,13 +7,13 @@ KERNEL=$(ls /lib/modules/ | grep bazzite | sort -V | tail -1)
 # Set up the build environment properly
 export KERNELDIR="/lib/modules/${KERNEL}/build"
 echo -e "\033[31mINSTALL XPADNEO\033[0m"
-set +u
+set +u #these are here prevent 'unbound variable' which doesn't make ANY sense.
 PREV_DIR=$(pwd) && echo -e "\033[33m$pwd\033[0m"
 set -u
 git clone https://github.com/atar-axis/xpadneo.git /tmp/xpadneo
 cd /tmp/xpadneo/hid-xpadneo
-#modified straight from xpadneo's makefile
 
+#modified straight from xpadneo's makefile
 tee makefile << 'EOF'
 KERNEL_SOURCE_DIR ?= /lib/modules/$(shell ls /lib/modules/ | grep bazzite | tail -1)/build
 LD := ld.bfd
@@ -55,7 +55,7 @@ if [[ -f "$FILE1" || -f "$FILE2" ]]; then
     echo -e "\033[38;5;208mXPADNEO INSTALLED\033[0m"
   else
     echo -e "\033[33;5mXPADNEO FAILED TO INSTALL\033[0m" && exit 1  
-fi
+fi #sanity check
 
 # Get kernel version and build initramfs
 KERNEL_VERSION="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' kernel)"
@@ -72,3 +72,7 @@ chmod 0600 "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
 
 #cd $PREV_DIR
 cd /
+
+#TODO: get cachyos kernel working
+#TODO: investigate if kmod packages are installed correctly after init-regen \
+# and consider switching to packaged variant of xpadneo
