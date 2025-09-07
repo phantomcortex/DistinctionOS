e9c5311f902be692d3ab5c2b04f7d8896fc78fdc
phantom <137958098+phantomcortex@users.noreply.github.com>
2025-09-07 04:58:17 -0600

    update many
    add config.sh, add passwordless-sudo, add distinction-firstrun service, update ujust recipes

---
+++ build_files/config.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+set -euo pipefail
+
+# script for things that don't really have a specific place
+
+# Change the default shell to zsh
+if [ -e /etc/default/useradd ];then
+ sed -i 's|SHELL=/bin/bash|SHELL=/usr/bin/zsh|' /etc/default/useradd
+fi 
+usermod -s /usr/bin/zsh root
+#
+
+# enable distictionos-firstrun
+systemctl enable distinction-firstrun.service
+
