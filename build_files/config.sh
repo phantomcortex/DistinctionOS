77e5ff6891704fb4926d1de5812beb6e3345db60
phantom <137958098+phantomcortex@users.noreply.github.com>
2025-09-07 05:37:17 -0600

    tpm-unlock auto-reenroll service added
    change untested.

--- build_files/config.sh
+++ build_files/config.sh
@@ -13,3 +13,7 @@ usermod -s /usr/bin/zsh root
 # enable distictionos-firstrun
 systemctl enable distinction-firstrun.service
 
+# Enable TPM monitoring services
+echo "Enabling TPM monitor services..."
+systemctl enable distinction-tpm-monitor.timer
+systemctl enable distinction-tpm-monitor.service
