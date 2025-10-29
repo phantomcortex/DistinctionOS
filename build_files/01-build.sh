81b8901562ea3cf9b8dae4a107a61b1f08f503de
phantom <phantom.github@proton.me>
2025-10-29 14:23:56 -0600

    Update 01-build.sh

--- build_files/01-build.sh
+++ build_files/01-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-set -euo pipefail
+set -uo pipefail
 
 # ============================================================================
 # DistinctionOS Build Script - Package Management & Repository Configuration
