bf0819b9339b0cbf7d3274c094e28ee46f25f63e
phantom <phantom.github@proton.me>
2026-02-10 10:20:32 -0700

    feat: Ship cachyos-kernel (#22)

---
+++ build_files/00-kernel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+set -euo pipefail
+
+# ============================================================================
+# Custom Kernel Installer
+# ============================================================================
+
+# Source utility functions
+source /ctx/95-utility-functions.sh
+
+for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-devel-matched kernel-modules-akmods; do 
+  rpm --erase $pkg --nodeps
+done 
+
+pushd /usr/lib/kernel/install.d 
+printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install 
+printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install 
+chmod +x 05-rpmostree.install 50-dracut.install 
+popd
+
+dnf5 -y copr enable bieszczaders/kernel-cachyos-lto
+dnf5 -y install kernel-cachyos-lto kernel-cachyos-lto-devel-matched
+dnf5 -y copr disable bieszczaders/kernel-cachyos-lto 
+dnf5 versionlock add kernel-cachyos-lto 
